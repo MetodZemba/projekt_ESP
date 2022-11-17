c027482f245a874be7b51b82f2db5c4da4e0c3a3
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-11-17 15:45:39 +0100

    commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         $sn1 = $_GET["a"];
         $txt = "Donald Duck\n";
 
-        $file1 = fopen("projekt_ESP/info_form.txt","a") or die("Unable to open file!");
+        $file1 = fopen("info_form.txt","a") or die("Unable to open file!");
         
         if ($sn1 == 4){
             fwrite($file1, $txt);
