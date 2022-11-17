ecdf48db450d134b976f722ac4e6f312cacd7128
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-11-17 15:18:17 +0100

    commit

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
         
         $sn1 = $_GET["a"];
-        
+        $txt = "Donald Duck\n";
+
         $file1 = fopen("info_form.txt","a") or die("Unable to open file!");
         
-        fwrite($file1, $sn1);
+        if ($sn1 == 4){
+            fwrite($file1, $txt);
+        }
         fclose($file1);
+
+        echo $txt;
+        echo $sn1;
     
     ?>
