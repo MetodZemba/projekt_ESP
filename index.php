78a487dc6384771abdb85ed10d6116b6413df511
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-11-17 16:09:51 +0100

    commit

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
         echo '<h1>index2.php web page with parameters</h1>';
 
         $sn1 = $_GET["a"];
