6a601fd95428b7a5a7e82d9f599e97b62d37c7ec
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-11-17 21:54:01 +0100

    commit

--- php_form/registration_form.php
+++ php_form/registration_form.php
@@ -4,7 +4,7 @@
         $sn1 = $_POST["firstname"];
         $sn2 = $_POST["lastname"];
 
-        $file1 = fopen("info_form.txt","a") or die("Unable to open file!");
+        $file1 = fopen("php_form//info_form.txt","a") or die("Unable to open file!");
         
         fwrite($file1, $sn1);
         fwrite($file1, $sn1);
