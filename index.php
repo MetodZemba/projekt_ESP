9ac0bf71927fa9d3494a819d73e854e17779c4b0
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-11-17 15:09:24 +0100

    committ

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
         
         $sn1 = $_GET["a"];
         
-        $file1 = fopen("info_form.txt","w") or die("Unable to open file!");
+        $file1 = fopen("info_form.txt","a") or die("Unable to open file!");
         
         fwrite($file1, $sn1);
         fclose($file1);
