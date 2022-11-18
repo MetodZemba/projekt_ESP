d30144fe3cf2e5f2b51edd345655a418d1bfa861
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-11-18 16:33:41 +0100

    commit

--- index.php
+++ index.php
@@ -79,16 +79,11 @@
 
 
 <?php
+        echo 'Form';
 
         $sn1 = $_POST["name"];
         $sn2 = $_POST["email];
-
-        $file1 = fopen("info_form.txt","a") or die("Unable to open file!");
-        
-        fwrite($file1, $sn1);
-        fwrite($file1, $sn1);
-        fclose($file1);
-        
+    
         echo $sn1;
         echo $sn2;
         
