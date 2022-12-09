2c1ecf4b89603dbb8ded9c35d5d66c3792ab202f
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-12-09 14:18:32 +0100

    commit

--- index.php
+++ index.php
@@ -15,26 +15,28 @@
 <p>Here in your form and text</p>
 <?php
     
-    echo '<h1> Index2.php  .txt</h1>';
+    echo '<h1> Index3.php  .txt</h1>';
 
         $sn1 = $_GET["a"];
-        $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+        $lines = [];
+
+        $file1 = fopen("sensors.txt","a") or die("Unable to open file!");
         $text1 = $sn1;
         
         fwrite($file1, $text1);
+        fwrite($file1, "\n");
         fclose($file1);
-
-        $file2 = fopen("sensors.txt","a") or die("Unable to open file!");
-        $text2 = $sn1;
         
-        fwrite($file2, $text2);
+        
+        $file2 = fopen("sensors.txt","r") or die("Unable to open file!");
+        if (!$file2) {
+            return;
+        }
+        while (!feof($file2)) {
+            $lines[] = fgets($ile2);
+        }
+        echo $lines;
         fclose($file2);
-        
-        
-        $file3 = fopen("sensors.txt","r") or die("Unable to open file!");
-        $text3 = fread($file3,filesize("sensors.txt"));
-        echo (int)$text3;
-        fclose($file3);
         /*
         if ($num == 22){
             echo "<img src='č_svetlo/1_0_0.png' class = 'img_1' >";
