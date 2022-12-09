370ed6517a88b3a2f661f538a5d3d5005a9d0dc3
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-12-09 15:39:01 +0100

    commi

--- index.php
+++ index.php
@@ -15,12 +15,12 @@
 <p>Here in your form and text</p>
 <?php
     
-    echo '<h1> Index3.php  .txt</h1>';
+    echo '<h1> Index2.php  .txt</h1>';
 
-        $sn1 = $_GET["a"];
+    $sn1 = $_GET["a"];
 
         
-
+    if($sn1 != 0){
         $file1 = fopen("sensors.txt","r+") or die("Unable to open file!");
         $text1 = $sn1;
         
@@ -33,6 +33,7 @@
         $text2 = fread($file2,filesize("sensors.txt"));
         echo (int)$text2;
         fclose($file2);
+
         /*
         if ($num == 22){
             echo "<img src='č_svetlo/1_0_0.png' class = 'img_1' >";
@@ -42,6 +43,7 @@
         }
         
         */
+    }
 ?>
 </body>
 </html>
