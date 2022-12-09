042b9d29a852b44370ce87a2d4f752a136706b8f
MetodZemba <114650910+MetodZemba@users.noreply.github.com>
2022-12-09 14:23:03 +0100

    commit

--- index.php
+++ index.php
@@ -15,10 +15,11 @@
 <p>Here in your form and text</p>
 <?php
     
-    echo '<h1> Index3.php  .txt</h1>';
+    echo '<h1> Index2.php  .txt</h1>';
 
         $sn1 = $_GET["a"];
-        $lines = [];
+
+        
 
         $file1 = fopen("sensors.txt","a") or die("Unable to open file!");
         $text1 = $sn1;
@@ -29,13 +30,8 @@
         
         
         $file2 = fopen("sensors.txt","r") or die("Unable to open file!");
-        if (!$file2) {
-            return;
-        }
-        while (!feof($file2)) {
-            $lines[] = fgets($ile2);
-        }
-        echo $lines;
+        $text2 = fread($file2,filesize("sensors.txt"));
+        echo (int)$text2;
         fclose($file2);
         /*
         if ($num == 22){
